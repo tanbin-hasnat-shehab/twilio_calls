78e019551e824869e42eba3cc19a45fcc7b77389
tanbin-hasnat-shehab <85411927+tanbin-hasnat-shehab@users.noreply.github.com>
2022-05-10 01:36:22 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 from twilio.rest import Client
 import streamlit as st
-client=Client('ACf0c9dd10f8ba0f28e2059ba419d712b0','16208f93e8280a26c4420d7572d4554a')
+
+st.header('Twilio make free calls')
+st.markdown('<a href="https://console.twilio.com/?frameUrl=/console">goto twilio dashboard</a>',unsafe_allow_html=True)
+SID=st.text_input('Account SID')
+auth=st.text_input('Auth Token')
+tw_ph=st.text_input('My Twilio phone number')
+st.markdown('<a href="https://console.twilio.com/us1/develop/phone-numbers/manage/verified?frameUrl=%2Fconsole%2Fphone-numbers%2Fverified%3Fx-target-region%3Dus1">see verified phone numbers you can call</a>',unsafe_allow_html=True)
+
+to_call=st.text_input('Enter Number')
+your_m=st.text_input('your message')
+
 
 
 def make_call():
+	client=Client(SID,auth)
 	call_now=client.calls.create(
 			twiml='<response><say>Do you Know me</say></response>',
-			from_='+19704699073',
-			to='+8801533687497'
+			from_=tw_ph,
+			to=to_call
 
 
 
@@ -15,13 +26,14 @@ def make_call():
 
 
 	messege=client.messages.create(
-			body='বেতন ঢুকছে??...হাসনাত',
-			from_='+19704699073',
-			to='+8801533687497'
+			body=your_m,
+			from_=tw_ph,
+			to=to_call
 
 
 
 			)
-call=st.button('call')
+call=st.button('SEND IT')
 if call:
+	st.success('call sent')
 	make_call()
